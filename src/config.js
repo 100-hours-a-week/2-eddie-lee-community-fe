091fcaf2e74d19b032ed5a5852256b73585880e9
kjr2020 <kjr202020@gmail.com>
2024-12-11 20:23:35 +0900

    docker-compose Test

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
     API_URL: '/api',
-    CDN_URL: 'http://dq5lukp1ccg9d.cloudfront.net'
+    //CDN_URL: 'http://dq5lukp1ccg9d.cloudfront.net'
+    CDN_URL: ''
 }
 
 export default config;
