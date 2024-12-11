d351055296b972128e99fc30f647b491fc1bc953
kjr2020 <kjr202020@gmail.com>
2024-12-11 23:55:57 +0900

    Dockerfile 추가

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
     API_URL: '/api',
-    //CDN_URL: 'http://dq5lukp1ccg9d.cloudfront.net'
-    CDN_URL: ''
+    CDN_URL: 'http://dq5lukp1ccg9d.cloudfront.net'
 }
 
 export default config;
